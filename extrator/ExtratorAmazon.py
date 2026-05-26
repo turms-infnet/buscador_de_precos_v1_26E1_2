7c6898d82198b7f5244b50d9ae7ec070615b0691
Tiago Silva <tiagoluizrs@gmail.com>
2026-05-26 20:31:23 -0300

    Database config (#7)
    * Database connection config
    
    * Continuação na configuração
    
    * ...
    
    * Finalizado banco de dados

--- extrator/ExtratorAmazon.py
+++ extrator/ExtratorAmazon.py
@@ -7,8 +7,8 @@ import time
 from utils import Money
 
 class ExtratorAmazon(ExtratorBase):
-    def __init__(self, nome_produto, logging):
-        super().__init__(nome_produto, "https://www.amazon.com.br", logging)
+    def __init__(self, nome_produto, logging, headless=False):
+        super().__init__(nome_produto, "https://www.amazon.com.br", logging, headless)
 
     def buscar_url(self, url_final, wait):
         _driver = super().buscar_url(url_final, wait)
@@ -50,18 +50,9 @@ class ExtratorAmazon(ExtratorBase):
 
             driver.close()
 
-            return {
-                "id_produto": None,
-                "preco": preco,
-                "plataforma": "AM",
-                "link": link,
-            }
+            return [None, preco, "AM", link]
+        
         except Exception as e:
             self.logging.error(f"Erro ao buscar produto: {e}")
-            return {
-                "id_produto": None,
-                "preco": "0.00",
-                "plataforma": "AM",
-                "link": None,
-            }
+            return [None, "0.00", "AM", None]
         
--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from dotenv import load_dotenv
 from extrator import ExtratorAmazon, ExtratorAmericanas, ExtratorMercadoLivre
 import logging
 from FileProcessor import Leitor, Escritor
-# import sentry_sdk
+from database.connection import (
+    open_connection, 
+    select_clientes, 
+    select_produtos, 
+    select_cliente_produtos, 
+    salvar_precos_encontrados
+)
+from selenium.webdriver.common.by import By
 
 from notificacao import Email
 
@@ -14,66 +21,70 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s no arquivo %(filename)s e função %(funcName)s na linha %(lineno)d: %(message)s",
 )
 
-# sentry_sdk.init(
-#     dsn=os.getenv("SENTRY_DSN"),
-#     send_default_pii=os.getenv("SEND_DEFAULT_PII"),
-# )
-
 load_dotenv()
 EMAIL = os.getenv("EMAIL")
 PASSWORD_APP = os.getenv("PASSWORD_APP")
 
-
-
-def main():
+def main(conn):
     carteiro = Email(EMAIL, PASSWORD_APP, logging)
 
-    leitorProdutos = Leitor("data/produtos.csv")
-    leitorClientes = Leitor("data/clientes.csv")
-    escritorProdutosEncontrados = Escritor("data/produtos_encontrados.csv")
-
-    lista_clientes = leitorClientes.ler_arquivo()
-    lista_produtos = leitorProdutos.ler_arquivo()
+    lista_clientes = select_clientes(conn, logging, True)
+    lista_produtos = select_produtos(conn, logging, True)
 
     lista_produtos_atualizados = []
 
     for produto in lista_produtos:
-        nome = produto["produto"]
-        extratorMl = ExtratorMercadoLivre(nome, logging)
+        nome = produto["nome"]
+        extratorMl = ExtratorMercadoLivre(nome, logging, {
+            "select": {
+                "type": By.CSS_SELECTOR,
+                "name": "button[class='andes-dropdown__trigger']"
+            },
+            "option": {
+                "type": By.CSS_SELECTOR,
+                "name": "li.andes-list__item.andes-list__item--size-medium:nth-of-type(2)"
+            },
+            "item": "ui-search-result__wrapper",
+            "price": "andes-money-amount__fraction",
+            "link": "poly-component__title"
+        })
         produtoMl = extratorMl.buscar_produto()
 
-        extratorAm = ExtratorAmazon(nome, logging)
-        produtoAm = extratorAm.buscar_produto()
+        # extratorAm = ExtratorAmazon(nome, logging)
+        # produtoAm = extratorAm.buscar_produto()
 
-        extratorAme = ExtratorAmericanas(nome, logging)
-        produtoAme = extratorAme.buscar_produto()
+        # extratorAme = ExtratorAmericanas(nome, logging)
+        # produtoAme = extratorAme.buscar_produto()
 
-        precoMl = produtoMl["preco"]
-        precoAm = produtoAm["preco"]
-        precoAme = produtoAme["preco"]
+        precoMl = produtoMl[1]
+        # precoAm = produtoAm[1]
+        # precoAme = produtoAme[1]
 
-        if precoMl < precoAm and precoMl < precoAme and precoMl != "0.00":
-            lista_produtos_atualizados.append(produtoMl)
-        elif precoAm < precoMl and precoAm < precoAme and precoAm != "0.00":
-            lista_produtos_atualizados.append(produtoAm)
-        elif precoAme !="0.00" :
-            lista_produtos_atualizados.append(produtoAme)
+        lista_produtos_atualizados.append(produtoMl)
+        # if precoMl < precoAm and precoMl < precoAme and precoMl != "0.00":
+        #     lista_produtos_atualizados.append(produtoMl)
+        # elif precoAm < precoMl and precoAm < precoAme and precoAm != "0.00":
+        #     lista_produtos_atualizados.append(produtoAm)
+        # elif precoAme !="0.00" :
+        #     lista_produtos_atualizados.append(produtoAme)
 
-        lista_produtos_atualizados[-1]["id_produto"] = produto["id"]
-        lista_produtos_atualizados[-1].update({"nome": nome})
+        lista_produtos_atualizados[-1][0] = produto["id_produto"]
+        lista_produtos_atualizados[-1].append(nome)
     
-    escritorProdutosEncontrados.escrever_arquivo(lista_produtos_atualizados)
+    salvar_precos_encontrados(conn, lista_produtos_atualizados, logging)
 
     for cliente in lista_clientes:
         email = cliente["email"]
-        ids_produtos = cliente["id_produtos"].split(",")
+        ids_produtos = select_cliente_produtos(conn, cliente["id_cliente"], logging, False)
         oferta_cliente = []
         for produto in lista_produtos_atualizados:
-            if produto["id_produto"] in ids_produtos:
+            if produto[0] in ids_produtos:
                 oferta_cliente.append(produto)
         
         carteiro.enviar_email(email, oferta_cliente)
 
 
 if __name__ == "__main__":
-    main()
+    conn = open_connection()
+    main(conn)
+    conn.close()
